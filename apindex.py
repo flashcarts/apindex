5479e9bad763c2061fb941b8bfd8e6229b5fdbdd
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-08-27 22:43:55 -0700

    apindex: complete rewrite
    Pretty much everything except the template HTMLs were rewritten.
    
    Now, it requires the output of `tree -Js` to function. This can either
    be a good thing or bad thing; Windows may not work anymore. On the
    other hand, this allows flexibility in that HTML files need not be in
    the actual directory of the files to function; this may be useful if
    one uses an identical directory structure, but the index files are
    served on another host (i.e. assets on S3, index somewhere else).
    
    Features added:
    - Allow a different base URL
        - Useful if actual files are hosted on a different host than the
          index.html files
    - Allow outputting the HTMLs to a directory of a user's choice
        - By default, it will use the `$PWD/site` directory
        - One can even pick the directory that the `tree -Js` command was
          run, if the files and the indexes are co-hosted
    
    Features removed:
    - Last modified time is removed
        - Purely due to laziness on my part; `tree` doesn't seem to have a
          structured output for modified time, the behaviour being
          inconsistent depending on the mod time
        - Support for it may be added at some point in the future

--- apindex.py
+++ apindex.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2017 Ognjen Galic
-# All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
+# apindex
+# Script that, given `tree -Js`, creates a static HTML directory listing
 #
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
+# Copyright (C) 2017 Ognjen Galic
+# Copyright (C) 2023 lifehackerhansol
+# SPDX-License-Identifier: BSD-2-Clause
 #
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import os
-import os.path
+import argparse
 import base64
-import math
-import sys
-
+import json
+import os
 from xml.dom.minidom import parseString
 
-import time
 
 VERSION = "@CPACK_PACKAGE_VERSION_MAJOR@.@CPACK_PACKAGE_VERSION_MINOR@"
 PREFIX = "@CMAKE_INSTALL_PREFIX@"
+
+
+def parseIconsDescription():
+    with open(f"{PREFIX}/share/apindex/icons.xml", "r") as f:
+        doc = parseString(f.read())
+    icons = []
+    for icon in doc.getElementsByTagName("icon"):
+        i = Icon(icon.getAttribute("file"))
+        for ex in icon.getElementsByTagName("ex"):
+            i.extensions.append(str(ex.firstChild.nodeValue))
+        icons.append(i)
+    return icons
+
+
+def readIconBase64(iconPath):
+    with open(iconPath, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode("ascii")
 
 
 class Icon:
@@ -44,166 +44,155 @@ class Icon:
         self.extensions = []
 
 
-class ResourceManager:
+class File():
+    def __init__(self, file, baseurl, output):
+        self.file = file
+        self.baseurl = baseurl
+        self.output = output
 
-    @staticmethod
-    def getFile(fileName):
-        return PREFIX + "/share/apindex/" + fileName
+    icons = parseIconsDescription()
 
-    @staticmethod
-    def readFile(fileName):
-        with open(ResourceManager.getFile(fileName), "r") as file:
-            data = file.read()
-        return str(data)
+    html = f"{PREFIX}/share/apindex/file.template.html"
 
-    @staticmethod
-    def writeFile(filePath, data):
-        with open(filePath, "w") as file:
-            file.write(data)
-
-    @staticmethod
-    def readFileBase64(fileName):
-        with open(ResourceManager.getFile(fileName), "rb") as file:
-            data = file.read()
-        return base64.b64encode(data).decode("ascii")
-
-    @staticmethod
-    def parseIconsDescription():
-        doc = parseString(ResourceManager.readFile("icons.xml"))
-        icons = []
-        for icon in doc.getElementsByTagName("icon"):
-            i = Icon(icon.getAttribute("file"))
-            for ex in icon.getElementsByTagName("ex"):
-                i.extensions.append(str(ex.firstChild.nodeValue))
-            icons.append(i)
-        return icons
-
-
-class File:
-
-    STATIC_FILE_HTML = ResourceManager.readFile("file.template.html")
-
-    FILE_ICON = ResourceManager.readFileBase64("img/file.png")
-    FOLDER_ICON = ResourceManager.readFileBase64("img/folder.png")
-    BACK_ICON = ResourceManager.readFileBase64("img/back.png")
-
-    ICONS = ResourceManager.parseIconsDescription()
+    def isDirectory(self):
+        return self.file["type"] == "directory"
 
     def getIcon(self):
+        if self.getFileName() == "..":
+            return readIconBase64(f"{PREFIX}/share/apindex/img/back.png")
 
-        if self.filename == "..":
-            return self.BACK_ICON
+        if self.isDirectory():
+            return readIconBase64(f"{PREFIX}/share/apindex/img/folder.png")
 
-        if self.isDir():
-            return self.FOLDER_ICON
-
-        for icon in self.ICONS:
+        for icon in self.icons:
             for ex in icon.extensions:
-                if self.filename.endswith(ex):
-                    return ResourceManager.readFileBase64("img/"+icon.file)
+                if self.getFileName().endswith(ex):
+                    return readIconBase64(f"{PREFIX}/share/apindex/img/{icon.file}")
 
-        return self.FILE_ICON
+        return readIconBase64(f"{PREFIX}/share/apindex/img/file.png")
 
-    def __init__(self, filename, root="."):
-        self.filename = File.stripCurrentDir(filename)
-        self.root = File.stripCurrentDir(root)
-
-    @staticmethod
-    def stripCurrentDir(path):
-        return path.replace("./", "").replace("/.", "")
-
-    def toHTML(self):
-        if self.isDir():
-            fileSize = "-"
-        else:
-            fileSize = str(math.floor(
-                os.path.getsize(self.getPath()) / 1000)) + " kB"
-        modifyTime = time.strftime('%d-%b-%Y %H:%M',
-                                   time.localtime(os.path.getmtime(self.getPath())))
-
-        return File.STATIC_FILE_HTML.replace("#FILENAME", self.filename) \
-            .replace("#MODIFIED", modifyTime).replace("#SIZE", str(fileSize)) \
-            .replace("#IMAGE", self.getIcon())
+    def getFileName(self) -> str:
+        return self.file["name"]
 
     def getPath(self):
-        return File.stripCurrentDir(self.root + "/" + self.filename)
-
-    def getPathFromRoot(self):
-        if self.filename == ".":
-            return "/"
+        if self.isDirectory():
+            return self.getFileName()
         else:
-            return "/" + self.getPath()
+            return f"{self.baseurl}/{self.getFileName()}"
 
-    def getFileName(self):
-        return self.filename
+    def getSize(self) -> str:
+        if self.isDirectory():
+            return "-"
+        byte_size: int = self.file["size"]
+        ret = "0 B"
+        if byte_size > (1 << 30):  # GiB
+            ret = f"{round(byte_size / (1 << 30), 2)} GiB"
+        elif byte_size > (1 << 20):  # MiB
+            ret = f"{round(byte_size / (1 << 20), 2)} MiB"
+        elif byte_size > (1 << 10):  # KiB
+            ret = f"{round(byte_size / (1 << 10), 2)} KiB"
+        else:
+            ret = f"{byte_size} B"
+        return ret
 
-    def isDir(self):
-        return os.path.isdir(self.getPath())
-
-    def getChildren(self):
-        children = []
-        for file in os.listdir(self.filename):
-            children.append(File(file, self.getPath()))
-        return children
-
-    def getParentDir(self):
-        return self.root
-
-
-class IndexWriter:
-
-    STATIC_FOOTER = ResourceManager.readFile("footer.template.html") \
-        .replace("#VERSION", VERSION)
-
-    @staticmethod
-    def writeIndex(startPath, title=None, footer=None):
-        filesRead = []
-        dirsRead = []
-        root = File(startPath)
-        html = ResourceManager.readFile("index.template.html")
-
-        if title is None:
-            title = root.getPathFromRoot()
-        if footer is None:
-            footer = IndexWriter.STATIC_FOOTER
-
-        # fill the details
-        html = html.replace("#TITLE", title)
-        html = html.replace("#FOOTER", footer)
-        html = html.replace("#DIR", root.getPathFromRoot())
-
-        # add the back dir
-        dirsRead.append(File("..").toHTML())
-
-        for file in root.getChildren():
-            # we do not want to index the index itself
-            if file.getFileName() == "index.html":
-                continue
-
-            if file.isDir():
-                dirsRead.append(file.toHTML())
-                IndexWriter.writeIndex(file.getPath(), title)
-            else:
-                filesRead.append(file.toHTML())
-
-        # fill in the file list
-        dirsRead.sort()
-        filesRead.sort()
-        html = html.replace("#GEN_DIRS", "".join(str(x) for x in dirsRead))
-        html = html.replace("#GEN_FILES", "".join(str(x) for x in filesRead))
-
-        # write the actual index file
-        print("Writing " + root.getPath() + "/index.html")
-        ResourceManager.writeFile(root.getPath() + "/index.html", html)
+    def genHTMLEntry(self):
+        with open(self.html, 'r') as f:
+            htmlEntry = f.read()
+        htmlEntry = htmlEntry.replace("#FILENAME", self.getFileName())
+        htmlEntry = htmlEntry.replace("#FILEPATH", self.getPath())
+        htmlEntry = htmlEntry.replace("#SIZE", self.getSize())
+        htmlEntry = htmlEntry.replace("#IMAGE", self.getIcon())
+        return htmlEntry
 
 
-def main():
-    if len(sys.argv) <= 1:
-        print("No root directory specified!")
-        exit(1)
+class Directory():
+    def __init__(self, directory, baseurl, output):
+        self.directory = directory
+        self.baseurl = baseurl
+        self.output = output
+        try:
+            os.mkdir(output)
+        except FileExistsError:
+            pass
 
-    IndexWriter.writeIndex(sys.argv[1])
+    html_head = f"{PREFIX}/share/apindex/index.template.html"
+    html_foot = f"{PREFIX}/share/apindex/footer.template.html"
+
+    def getDirName(self):
+        return self.directory["name"]
+
+    def write(self):
+        with open(self.html_head, 'r') as f:
+            htmlContent = f.read()
+
+        # initial setup
+        htmlContent = htmlContent.replace("#DIR", self.baseurl)
+        htmlContent = htmlContent.replace("#TITLE", self.baseurl)
+        htmlContentDir = ""
+        htmlContentFile = ""
+
+        # add link to go to previous dir
+        back_directory = File({"type": "directory", "name": ".."}, self.baseurl, self.output)
+        htmlContentDir += back_directory.genHTMLEntry()
+
+        # check if directory actually has anything to add
+        if "contents" in self.directory:
+            # loop through files and dirs
+            for i in self.directory["contents"]:
+                file = File(i, self.baseurl, self.output)
+                if file.isDirectory():
+                    # spawn new class and write those first
+                    subdirectory = Directory(i, f"{self.baseurl}/{i['name']}", f"{self.output}/{i['name']}")
+                    subdirectory.write()
+                    htmlContentDir += file.genHTMLEntry()
+                else:
+                    htmlContentFile += file.genHTMLEntry()
+
+        htmlContent = htmlContent.replace("#GEN_DIRS", htmlContentDir)
+        htmlContent = htmlContent.replace("#GEN_FILES", htmlContentFile)
+
+        with open(self.html_foot, "r") as f:
+            htmlContent = htmlContent.replace("#FOOTER", f.read()).replace("#VERSION", VERSION)
+        with open(f"{self.output}/index.html", "w") as f:
+            f.write(htmlContent)
 
 
 if __name__ == "__main__":
-    main()
+    description = "apindex - Script that, given `tree -Js`, creates a static HTML directory listing"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("tree",
+                        metavar="<output of `tree -Js>`",
+                        type=str,
+                        nargs=1,
+                        help="root directory of files"
+                        )
+    parser.add_argument("-b",
+                        "--baseurl",
+                        metavar="http://localhost/",
+                        type=str,
+                        nargs=1,
+                        help="Optional: baseurl, use if file is on another host from dir list"
+                        )
+    parser.add_argument("-o",
+                        "--out",
+                        metavar="site",
+                        type=str,
+                        nargs=1,
+                        help="Output directory (default: 'site')"
+                        )
+
+    args = parser.parse_args()
+
+    baseurl = ""
+    if args.baseurl:
+        baseurl = args.baseurl[0]
+
+    output = "site"
+    if args.out:
+        output = args.out[0]
+
+    with open(args.tree[0], 'r') as f:
+        dirtree = json.load(f)
+
+    rootdir = Directory(dirtree[0], baseurl, output)
+    rootdir.write()
